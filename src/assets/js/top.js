a48ce214086060553cc2788e3ccaa41c7efa2cdb
kikuchi_tomoki <t.kikuchi@snowrobin.jp>
2019-06-03 13:57:40 +0900

    パラメータを渡した時に初期表示のタブを変更する処理を追加

--- src/assets/js/top.js
+++ src/assets/js/top.js
@@ -1,22 +1,16 @@
 import $ from 'jquery';
 
 $(() => {
-  const $tabContent = $('.tabContent');
   const $tab = $('.tab');
 
-  $tabContent.hide();
-  $tabContent.eq(0).show();
-
   setSnsShareLink();
+  setInitialTab();
 
+  // タブの切替を行う
   $tab.on('click', function(e) {
     e.preventDefault();
     const index = $tab.index(this);
-    $tab.removeClass('selected');
-    $tab.eq(index).addClass('selected');
-
-    $tabContent.hide();
-    $tabContent.eq(index).show();
+    selectTab(index);
   });
 });
 
@@ -29,7 +23,7 @@ function setSnsShareLink() {
     const href = $(this).attr('href');
     const shareUrl = `https://quirky-mcclintock-c77b60.netlify.com/${href}`;
 
-    console.log(shareUrl);
+    console.log(`shareUrl::`, shareUrl);
     openPopUpWin(`https://www.facebook.com/sharer/sharer.php?u=${shareUrl}`);
   });
 
@@ -40,7 +34,7 @@ function setSnsShareLink() {
     const siteTitle = 'タイトル';
     const hashTags = ['ハッシュタグ1', 'ハッシュタグ2'];
 
-    console.log(shareUrl);
+    console.log(`shareUrl::`, shareUrl);
 
     openPopUpWin(`http://twitter.com/share?text=${siteTitle}&url=${shareUrl}&hashtags=${hashTags.join(',')}`);
   });
@@ -48,4 +42,49 @@ function setSnsShareLink() {
 
 function openPopUpWin(url) {
   window.open(url, 'shareWin', 'width=600,height=400');
+}
+
+/**
+ * GETパラメータを取得する
+ */
+function getUrlVars() {
+  const vars = {};
+  const params = location.search.substring(1).split('&');
+  params.forEach(param => {
+    let key = '';
+    const value = param.slice(param.indexOf('=', 0) + 1);
+    const keySearch = param.search(/=/);
+    if (keySearch !== -1) {
+      key = param.slice(0, keySearch);
+    }
+    if (key !== '') {
+      vars[key] = decodeURI(value);
+    }
+  });
+  return vars;
+}
+
+/**
+ * 初期表示のタブの設定を行う
+ */
+function setInitialTab() {
+  const params = getUrlVars();
+  const tabIndex = params['tab'] ? params['tab'] : 0;
+  console.log(`param:`, params);
+  console.log(`tabIndex:`, tabIndex);
+  selectTab(tabIndex);
+}
+
+/**
+ * タブの切替を行う
+ * @param index
+ */
+function selectTab(index) {
+  const $tab = $('.tab');
+  const $tabContent = $('.tabContent');
+  $tab.removeClass('selected');
+  $tab.eq(index).addClass('selected');
+
+  $tabContent.hide();
+  $tabContent.eq(index).show();
 }
